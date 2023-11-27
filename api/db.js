eee9619c4f0a9840f8e7c44b09e17072e9b32214
mathyled <mathiasimagine@gmail.com>
2023-11-26 22:24:26 -0300

    rf db name

--- api/db.js
+++ api/db.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("pokemons", DB_USER, DB_PASSWORD, {
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   dialect: "postgres",
 });
